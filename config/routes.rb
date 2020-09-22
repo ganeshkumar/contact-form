820ad14c78bece2b971ddbf5cd894e6d5cd15f28
ganesh <ganesh.dc@gmail.com>
2020-09-22 19:13:01 +0530

    Added routes to redirect to contact form and corrected database.yml file db name, password

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  def new_contact
+    redirect('ContactFrom/index.html')
+  end
+
+  get '/contact', to: new_contact
+  root to: new_contact
+
+  match '*path',  to: redirect('/404'), via: :all
 end
