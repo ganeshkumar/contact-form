98c4c10e36df483d1fbd6eb42817e4854f65a9fc
ganesh <ganesh.dc@gmail.com>
2020-09-22 20:30:57 +0530

    Added rspec, simplecov, database_cleaner, factory_girl gems and configured it

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module ContactForm
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => true,
+        :routing_specs => true,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
